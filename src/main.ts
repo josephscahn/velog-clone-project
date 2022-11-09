875b774642d8956c4ebd569b96dd19bd8281b02f
hyunjung <hhhj.1486@gmail.com>
2022-11-09 10:30:30 +0900

    Fix: 초기 설정 수정
    1. typeorm 연동 방식 변경
    → ormconfig.ts 참조하는 방식에서 ormconfig.json을 동적으로 생성해서 참조하는 방식으로 변경
    
    2. 유효성 검사 관련
    main.ts 에 useGlobalPipes 추가하여 class-validator 사용 시 좀 더 간결하게 유효성 검사 가능 (기존은 controller에 추가하여 사용하였음)
    
    3. .ENV에 SERVER_POST 프로퍼티 추가하여 서버 PORT 숨김

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,39 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from 'config/config.service';
+import * as fs from 'fs';
+
+require('dotenv').config();
 
 async function bootstrap() {
+  await makeOrmConfig();
 
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    })
+  );
+
+  await app.listen(process.env.SERVER_POST);
 }
+
+async function makeOrmConfig() {
+  const configService = new ConfigService(process.env);
+  const typeormConfig = configService.getTypeOrmConfig();
+
+  if (fs.existsSync('ormconfig.json')) {
+    fs.unlinkSync('ormconfig.json');
+  }
+
+  fs.writeFileSync(
+    'ormconfig.json',
+    JSON.stringify(typeormConfig, null, 2)
+  );
+}
+
 bootstrap();
